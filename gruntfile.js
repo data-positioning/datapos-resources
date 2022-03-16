b5bfd667f45d86e18653ed453586550a4b43859a
Jonathan Terrell <terrell.jm@gmail.com>
2022-03-16 14:15:30 +0100

    new files

--- gruntfile.js
+++ gruntfile.js
@@ -20,7 +20,6 @@ module.exports = function init(grunt) {
         run: {
             audit: { args: ['npm', 'audit'], cmd: 'npx' },
             build: { args: ['WARNING: Build is NOT implemented.'], cmd: 'echo' },
-            copyBuildKitsToFirebase: { args: ['cp', '-r', 'public/buildKits/*', `${firebaseStorageUrl}buildKits/`], cmd: 'gsutil' },
             copyDocumentationToFirebase: { args: ['cp', '-r', 'public/documentation/*', `${firebaseStorageUrl}documentation/`], cmd: 'gsutil' },
             copyEmulatorsToFirebase: { args: ['cp', '-r', 'public/emulators/*', `${firebaseStorageUrl}emulators/`], cmd: 'gsutil' },
             copyFileStoreToFirebase: { args: ['cp', '-r', 'public/fileStore/*', `${firebaseStorageUrl}fileStore/`], cmd: 'gsutil' },
